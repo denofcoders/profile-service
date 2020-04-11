c861201717c64777ec930af37a264213657059a3
Den of Coders <social.denofcoder@gmail.com>
2020-04-11 20:22:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot Config Server Demo
+
+## written tutorial can be found <a href="https://denofcoders.com/implementing-spring-boot-config-server/">here</a>
---
+++ src/test/java/com/denofcoders/profileservice/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denofcoders.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
